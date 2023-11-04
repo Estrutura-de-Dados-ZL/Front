0d6aecd5d3e0e591ebeb3f036805c7d9d9899cd0
Guilherme Serafim Bonfim <guilhermeserafimbonfim@hotmail.com>
2023-11-04 16:45:42 -0300

    atualiza imports app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {MenubarModule} from 'primeng/menubar';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {InputSearchComponent} from './components/input-search/input-search.component';
+import {FilterSearchComponent} from './components/filter-search/filter-search.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		HomeComponent,
+		NavbarComponent,
+		InputSearchComponent,
+		FilterSearchComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+		MenubarModule,
+		FormsModule,
+		InputTextModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
