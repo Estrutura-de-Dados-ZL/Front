1f0366a83f920627ce26a0ea80a2f83e1fb14b2d
Guilherme Serafim Bonfim <guilhermeserafimbonfim@hotmail.com>
2023-11-04 11:23:37 -0300

    install eslint and apply rules

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'xo',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'new-cap': 'off',
+		'object-curly-spacing': ['error', 'always'],
+	},
+};
